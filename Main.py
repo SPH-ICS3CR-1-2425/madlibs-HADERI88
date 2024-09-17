dd32e8d040cdd6b3db37dbadfc60ee94c2e9b5e8
HADERI88 <haderi.haile@stu.ocsb.ca>
2024-09-17 11:13:14 -0400

    koolAid
    koolAid

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
 #Ask the user for at least ten questions
 #Read in each answer and store it in a variable
 #Print out the result
+print("give me a verb")
+verb1 = input()
+print("give me a person")
+person = input()
+# ("give me other verb")
+verb1 = input()
+# ("talk")
+# ("give me a drink")
 
-print("Welcome to Mad Libs!")
-print("Give me a name: ")
-name=input()
-print("Give me a noun")
-noun1=input()
-print("One day",name,"wrote a",noun1,"for class!")
+# ("KoolAid")
+print(person,"was",verb1)
