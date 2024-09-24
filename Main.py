6cc3c50849351b0dc411ff4908d64cc51ffaecda
HADERI88 <haderi.haile@stu.ocsb.ca>
2024-09-24 10:26:22 -0400

    I did stuff

--- Main.py
+++ Main.py
@@ -2,13 +2,14 @@
 #Read in each answer and store it in a variable
 #Print out the result
 print("give me a verb")
-verb1 = input()
+verb1=input()
 print("give me a person")
-person = input()
-# ("give me other verb")
-verb1 = input()
-# ("talk")
-# ("give me a drink")
+person =input()
+print("give me a food")
+food=input()
+print ("give me a verb")
+input()
 
-# ("KoolAid")
-print(person,"was",verb1)
+print()
+
+print(person,"walk up to the",food)
